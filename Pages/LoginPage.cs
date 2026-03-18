ac4013a4e6aa6bbccfaf8c25f3170f631638fe89
Poornaiah-4236 <168422073+Poornaiah-4236@users.noreply.github.com>
2026-03-18 17:17:52 +0530

    Update LoginPage.cs
    Unwanted Script deleted

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -22,71 +22,7 @@ namespace TechMehendra.Pages
         {
             this.driver = driver;
           
-        }
-
-        
-       /* public void EnterUserName(string text)
-        {
-            WaitForElement(userName, 30).SendKeys(text);
-
-
-
-        }
-        public void EnterPassword(string text)
-        {
-            WaitForElement(password, 30).SendKeys(text);
-
-        }
-        public void ClickLogin()
-        {
-            WaitForElement(btnLogin, 30).Click();
-        }
-        public String GetErrorMessage()
-        {
-            return WaitForElement(errorMessage, 60).Text;
-
-        }
-        public IWebElement WaitforElementDisplayed(By locator)
-        {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-
-            return wait.Until(d =>
-            {
-                try
-                {
-                    IWebElement element = d.FindElement(locator);
-                    return element.Displayed ? element : null;
-                }
-                catch
-                {
-                    return null;
-                }
-            });
-        }
-        private IWebElement WaitForElement(By locator, int time)
-        {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(time));
-
-            return wait.Until(d =>
-            {
-                try
-                {
-                    var el = d.FindElement(locator);
-                    return el.Displayed ? el : null;
-                }
-                catch
-                {
-                    return null;
-                }
-            });
-        }
-        public void Login(string username, string pwd)
-        {
-            EnterUserName(username);
-            EnterPassword(pwd);
-           ClickLogin();
-        }
-        */
+        }      
     }
  }
     
